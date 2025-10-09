38cc9e620d04cfc712a3c5f1fe4e8aa1b001e6fb
GIang0212 <gianglun02122004@gmail.com>
2025-10-09 09:07:33 +0700

    update order

--- Backend/Data/AppDbContext.cs
+++ Backend/Data/AppDbContext.cs
@@ -16,7 +16,8 @@ namespace Backend.Data
         public DbSet<Vouncher> Vounchers { get; set; } = default!;
         public DbSet<Cart> Carts { get; set; } = default!;
         public DbSet<CartItem> CartItems { get; set; } = default!; // ← Lớp số ít
-
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
         public override int SaveChanges()
         {
             ApplyProductRules();
@@ -53,6 +54,10 @@ namespace Backend.Data
                 .WithMany(p => p.CartItems!)    // ← cần nav ngược trong Product
                 .HasForeignKey(i => i.ProductId)
                 .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<Order>()
+                  .HasMany(o => o.Items)
+                    .WithOne(oi => oi.Order)
+                .HasForeignKey(oi => oi.OrderId);       
         }
 
         private void ApplyProductRules()
